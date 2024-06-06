63b6b8bbf74d73ea4df6f0f31d6d2bd29904ca4a
benahahumber <n01713464@humber.ca>
2024-06-06 15:31:11 -0400

    start

---
+++ JavaLab5/src/FactorialCalculator.java
@@ -0,0 +1,21 @@
+public class FactorialCalculator {
+    int myInt;
+    int myRes;
+
+    public int getMyRes() {
+        return myRes;
+    }
+
+    public void setMyRes(int myRes) {
+        this.myRes = myRes;
+    }
+
+    public int getMyInt() {
+        return myInt;
+    }
+
+    public void setMyInt(int myInt) {
+        this.myInt = myInt;
+    }
+
+}
